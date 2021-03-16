a06b20ac95b7a62c1e4008532376a026c2dc2812
projetEphec1 <nadiam005@yahoo.fr>
2021-03-16 23:46:07 +0100

    add homepage

---
+++ react_creche/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navigation />
+     
+     <h1> Bienvenu sur notre page </h1>
+    </div>
+  );
+};
+
+export default Home;
