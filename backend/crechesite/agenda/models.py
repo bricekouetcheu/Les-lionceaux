48bf543ce3572165976158e911e9f50566af5d4b
nadia <nadiam005@yahoo.fr>
2021-07-16 22:09:10 +0200

    implement models and admin agenda

--- backend/crechesite/agenda/models.py
+++ backend/crechesite/agenda/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from utilisateurs.models import Groupe
 
 # Create your models here.
+
+
+class Agenda(models.Model):
+    """Model definition for Agenda."""
+
+    # TODO: Define fields here
+    date_evenement = models.DateField()
+    heure = models.TimeField()
+    titre_evenement = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    groupe = models.ManyToManyField(Groupe, related_name="agenda_groupe")
+    visible = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_upd = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta definition for Agenda."""
+
+        app_label = "agenda"
+        verbose_name = "Agenda"
+        verbose_name_plural = "Agenda"
+
+    def __str__(self):
+        """Unicode representation of Agenda."""
+        return self.titre_evenement
